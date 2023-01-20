eaf488e389486e99f2aaf22105ade4cd85e80651
hirmaria <girman.mv@gmail.com>
2023-01-20 09:33:18 +0100

    Removed React

--- src/CurrentDate.js
+++ src/CurrentDate.js
@@ -1,14 +1,20 @@
-import React from "react";
-
 export default function CurrentDate() {
   let newDate = new Date();
   let hour = newDate.getHours();
   let minute = newDate.getMinutes();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
   let day = days[newDate.getDay()];
 
-  if ({ hour } < 10) {
-    if ({ minute } < 10) {
+  if (hour < 10) {
+    if (minute < 10) {
       return `${day}, 0${hour}:0${minute}`;
     } else {
       return `${day}, 0${hour}:${minute}`;
