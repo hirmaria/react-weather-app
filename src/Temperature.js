b52a70777c01d47fdab54563fca8d3434f1e12df
hirmaria <girman.mv@gmail.com>
2023-01-25 09:16:27 +0100

    Finished Temperature.js
    Now conversion is working

--- src/Temperature.js
+++ src/Temperature.js
@@ -2,46 +2,45 @@ import "./Temperature.css";
 import React, { useState } from "react";
 
 export default function Temperature(props) {
-  let [temperature, setTemperature] = useState(props.temp);
-  let [celcius, setCelcius] = useState(true);
+  let [units, setUnits] = useState("celsius");
 
   function handleFahrenheit(event) {
     event.preventDefault();
-    setCelcius(false);
-    setTemperature(Math.round(temperature * 1.8 + 32));
+    setUnits("fahrenheit");
   }
-
-  function handleCelcius(event) {
+  function handleCelsius(event) {
     event.preventDefault();
-    setCelcius(true);
-    setTemperature(props.temp);
+    setUnits("celsius");
   }
 
-  return (
-    <span className="Temperature">
-      <span className="current-temp">{temperature}</span>
-      <span className="units">
-        {{ celcius } ? (
-          <a href="/" className="inactive">
-            °C
-          </a>
-        ) : (
-          <a href="/" className="active" onClick={handleCelcius}>
-            °C
-          </a>
-        )}
-
-        {" |"}
-        {{ celcius } ? (
-          <a href="/" className="active" onClick={handleFahrenheit}>
-            °F
-          </a>
-        ) : (
-          <a href="/" className="inactive">
-            °F
-          </a>
-        )}
+  if (units === "celsius") {
+    return (
+      <span className="Temperature">
+        <span className="current-temp">{props.temp}</span>
+        <span className="units">
+          <span>
+            °C|
+            <a href="/" className="active" onClick={handleFahrenheit}>
+              °F
+            </a>
+          </span>
+        </span>
       </span>
-    </span>
-  );
+    );
+  } else {
+    let temp = Math.round(props.temp * 1.8 + 32);
+    return (
+      <span className="Temperature">
+        <span className="current-temp">{temp}</span>
+        <span className="units">
+          <span>
+            <a href="/" className="active" onClick={handleCelsius}>
+              °C
+            </a>
+            |°F
+          </span>
+        </span>
+      </span>
+    );
+  }
 }
