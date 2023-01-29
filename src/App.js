20ea784331913a09697c6efe1b787e10eeff5aed
hirmaria <girman.mv@gmail.com>
2023-01-29 11:35:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,11 +9,30 @@ export default function App() {
         <Weather defaultCity="Kyiv" />
 
         <footer className="mt-5">
-          This project was coded by
-          <a href="https://github.com/hirmaria"> Maria Hirman</a>{" "}
+          This project was coded by{" "}
+          <a
+            href="https://github.com/hirmaria"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Maria Hirman
+          </a>{" "}
           <span>and is open-source code on</span>{" "}
-          <a href="https://github.com/hirmaria/react-weather-app">GitHub</a>
-          <span>and hosted on</span>
+          <a
+            href="https://github.com/hirmaria/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          <span>and hosted on</span>{" "}
+          <a
+            href="https://calm-dragon-6c2e6a.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
         </footer>
       </div>
     </div>
